ee7760f95023eed9e0887067d26988f3a87f6c37
Isaac <mugagga10@gmail.com>
2024-04-01 10:49:43 -0700

    deletions

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""This module implement a rule that returns the status of the application"""
-from flask import jsonify
-import models
+""" Status Route for the Api """
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -11,20 +11,23 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route("/status", strict_slashes=False)
-def view_status():
-    """View function that return a json message"""
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def return_status():
+    """ Response for a successful fetch """
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
-def view_stats():
-    """Veiw function that retrieves the number of each object by type"""
-    return jsonify({
-        "amenities": models.storage.count(Amenity),
-        "cities": models.storage.count(City),
-        "places": models.storage.count(Place),
-        "reviews": models.storage.count(Review),
-        "states": models.storage.count(State),
-        "users": models.storage.count(User)
-    })
+@app_views.route('/stats', strict_slashes=False)
+def get_stats():
+    """ Returns number of objects of each type """
+    objects = {
+            'amenities': Amenity,
+            'cities': City,
+            'places': Place,
+            'reviews': Review,
+            'states': State,
+            'users': User
+            }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
